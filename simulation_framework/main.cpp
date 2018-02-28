3ed5749de65fa853a76d7ec365454d5e89f868cb
Vladislav Odobesku <vladyslavo@compatibl.com>
2018-02-28 22:47:43 +0200

    Upd.

--- simulation_framework/main.cpp
+++ simulation_framework/main.cpp
@@ -6,15 +6,23 @@
 #include <smpp/mmsim.hpp>
 #include <smpp/smpp.hpp>
 #include <smpp/task.hpp>
+#include <smpp/processor.hpp>
 
 
 int main()
 {
+	typedef smpp::SimpleTask task;
+	typedef smpp::Processor processor;
 	const double nominal_mips = 10000000000.0;
-	const std::vector<double> MIPS = { nominal_mips, nominal_mips, nominal_mips, nominal_mips };
+	std::vector<processor> mips;
+	mips.emplace_back(nominal_mips);
+	mips.emplace_back(nominal_mips);
+	mips.emplace_back(nominal_mips);
+	mips.emplace_back(nominal_mips);
 
-	auto tasks = smpp::mmsim::create_tasks<smpp::SimpleTask>(5000, { 2,2 });
-	auto res = smpp::simulate<smpp::SimpleTask, std::less<smpp::SimpleTask>, std::less<>, true>(MIPS, std::move(tasks));
+	auto tasks = smpp::mmsim::create_tasks<task>(5000, { 2,2 });
+	auto res = smpp::simulate<processor,task, task::small_first, processor::slow_first>(mips, std::move(tasks));
+
 	std::cout << res.begin()->expected_completition_time << std::endl;
 	return 0;
 }
--- simulation_framework/smpp/smpp.hpp
+++ simulation_framework/smpp/smpp.hpp
@@ -10,8 +10,10 @@
 
 namespace smpp
 {
-	template<class Task, class task_comparator, class mips_comparator, bool shuffle>
-	auto simulate(std::vector<double> mips, std::vector<Task>&& tasks)
+	template<class Processor, class Task, class TaskComparator, class ProcessorComparator,
+		
+		bool shuffle = false>
+	auto simulate(std::vector<Processor> procs, std::vector<Task>&& tasks)
 	{
 		std::vector<Task> tasks_to_process = std::move(tasks);
 
@@ -21,20 +23,19 @@ namespace smpp
 			std::mt19937 g(rd());
 			std::shuffle(tasks_to_process.begin(), tasks_to_process.end(), g);
 		}
-		
 
 		// sort tasks
-		std::sort(tasks_to_process.begin(), tasks_to_process.end(), task_comparator());
+		std::sort(tasks_to_process.begin(), tasks_to_process.end(), TaskComparator());
 		// sort processors
-		std::sort(mips.begin(), mips.end(), mips_comparator());
+		std::sort(procs.begin(), procs.end(), ProcessorComparator());
 
 		priority_queue<task_completition<Task>, typename task_completition<Task>::later_first> p_queue;
 		std::list<task_completition<Task>> processed_tasks;
 
 		auto task_iterator = tasks_to_process.begin();
-		for (size_t i = 0; i < mips.size() && task_iterator != tasks_to_process.end(); ++i)
+		for (size_t i = 0; i < procs.size() && task_iterator != tasks_to_process.end(); ++i)
 		{
-			auto time_to_process = task_iterator->complexity / mips[i];
+			auto time_to_process = procs[i].time_to_complete(task_iterator->complexity);
 			p_queue.emplace(time_to_process, i, std::move(*task_iterator));
 			++task_iterator;
 		}
@@ -44,7 +45,7 @@ namespace smpp
 			auto tk = p_queue.pop();
 			if (task_iterator != tasks_to_process.end())
 			{
-				auto time_to_process = task_iterator->complexity / mips[tk.worker_index];
+				auto time_to_process = procs[tk.worker_index].time_to_complete(task_iterator->complexity);
 				p_queue.emplace(tk.expected_completition_time + time_to_process, tk.worker_index, std::move(*task_iterator));
 				++task_iterator;
 			}
