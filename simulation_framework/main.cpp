5bed5b315ce3f6a6dbc8386c4ec467ee70193045
Vladislav Odobesku <vladyslavo@compatibl.com>
2018-04-14 11:01:46 +0300

    Added custom slices.

--- simulation_framework/main.cpp
+++ simulation_framework/main.cpp
@@ -1,13 +1,13 @@
 #include <list>
 #include <vector>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <functional>
 
-#include <boost/algorithm/string.hpp>    
+#include <boost/algorithm/string.hpp>
 #include <boost/program_options.hpp>
 #include <valarray>
-namespace po = boost::program_options;
 
 #include <smpp/mmsim.hpp>
 #include <smpp/smpp.hpp>
@@ -15,11 +15,12 @@ namespace po = boost::program_options;
 #include <smpp/processor.hpp>
 #include <smpp/task_processor.hpp>
 
+namespace po = boost::program_options;
 
 int main(int argc, char* argv[])
 {
-    typedef smpp::SimpleTask				task;
-    typedef smpp::Processor					processor;
+    typedef smpp::SimpleTask                task;
+    typedef smpp::Processor                 processor;
     typedef smpp::TaskProcessorWithTransfer	task_processor;
 
     typedef std::function<std::vector<double>(const size_t, const std::vector<smpp::Processor>& procs, std::vector<smpp::SimpleTask>&&, const smpp::TaskProcessor&)> fsimulator;
@@ -28,25 +29,25 @@ int main(int argc, char* argv[])
     {
         po::options_description desc("Allowed options");
         desc.add_options()
-            ("help"					, "produce help message")
-            ("sim_log"				, "log simulation data")
+            ("help"                 , "produce help message")
+            ("sim_log"              , "log simulation data")
             //general simulation params
-            ("problem_size"			, po::value<size_t>()->required()							, "problem size"												)
-            ("nominal_mips"			, po::value<double>()->default_value(1e10)					, "nominal mips value"											)
-            ("mips"					, po::value<std::vector<double>>()->required()->multitoken(), "cores mips values as multiplication of nominal"              )
-            ("task_priority"		, po::value<std::string>()->default_value("min")			, "task scheduling priority"                                    )
-            ("proc_priority"		, po::value<std::string>()->default_value("min")			, "processor choosing priority"                                 )
+            ("problem_size"         , po::value<size_t>()->required()                           , "problem size"                                                )
+            ("nominal_mips"         , po::value<double>()->default_value(1e10)                  , "nominal mips value"                                          )
+            ("mips"                 , po::value<std::vector<double>>()->required()->multitoken(), "cores mips values as multiplication of nominal"              )
+            ("task_priority"        , po::value<std::string>()->default_value("min")            , "task scheduling priority"                                    )
+            ("proc_priority"        , po::value<std::string>()->default_value("min")            , "processor choosing priority"                                 )
             // task processor
-            ("bandwidth"			, po::value<double>()->default_value(1e8)					, "bandwidth with each processing unit (one value for all)"     )
-            ("ping"					, po::value<double>()->default_value(1e-5)					, "ping with each processing unit (one value for all)"          )
+            ("bandwidth"            , po::value<double>()->default_value(1e8)                   , "bandwidth with each processing unit (one value for all)"     )
+            ("ping"                 , po::value<double>()->default_value(1e-5)                  , "ping with each processing unit (one value for all)"          )
             // slice params
-            ("slices"				, po::value<std::vector<size_t>>()->multitoken()->required(), "slice params (min slice, max slice, step)"                   )
-            ("fix_first"			, po::value<size_t>()->default_value(0)                     , "fixed first player strategy"                                 )
+            ("slices"               , po::value<std::vector<size_t>>()->multitoken()->required(), "slice params (min slice, max slice, step)"                   )
+            ("fix_first"            , po::value<size_t>()->default_value(0)                     , "fixed first player strategy"                                 )
 
-            ("randomize_count"		, po::value<size_t>()->default_value(1)						, "how many times to simulate with shufling"					)
-            ("single_player"		, po::value<bool>()->default_value(false)					, "make single player simulation"								)
+            ("randomize_count"      , po::value<size_t>()->default_value(1)                     , "how many times to simulate with shufling"                    )
+            ("single_player"        , po::value<bool>()->default_value(false)                   , "make single player simulation"                               )
 
-            ("output"				, po::value<std::string>()->default_value("results.txt")	, "output file"													)
+            ("output"               , po::value<std::string>()->default_value("results.txt")    , "output file"                                                 )
             ;
 
         po::variables_map vm;
@@ -95,12 +96,22 @@ int main(int argc, char* argv[])
         const double ping		= vm["ping"].as<double>();
         std::unique_ptr<task_processor> tp = std::make_unique<task_processor>(bandwidth, ping);
 
-        const std::vector<size_t> slices = vm["slices"].as<std::vector<size_t>>();
-        if(slices.size() != 3)
-            throw po::validation_error(po::validation_error::invalid_option_value, "slices");
-        const size_t slice_start    = slices[0];
-        const size_t slice_end      = slices[1];
-        const size_t slice_step     = slices[2];
+        const std::vector<size_t> slices_arr = vm["slices"].as<std::vector<size_t>>();
+        std::vector<size_t> slices;
+        if (slices_arr.size() == 3)
+        {
+            const size_t slice_start    = slices_arr[0];
+            const size_t slice_end      = slices_arr[1];
+            const size_t slice_step     = slices_arr[2];
+            slices.reserve((slice_end - slice_start + 1) / slice_step);
+            for (size_t i = slice_start; i < slice_end; i += slice_step)
+                slices.push_back(i);
+        }
+        else
+        {
+            slices = slices_arr;
+        }
+
         const size_t fix_first      = vm["fix_first"].as<size_t>();
 
         const size_t	randomize_count = vm["randomize_count"].as<size_t>();
@@ -114,12 +125,11 @@ int main(int argc, char* argv[])
         if (fname == "auto")
         {
             std::stringstream ss;
-            ss.precision(5);
             ss << "sim_"    << proc_priority << "_" << task_priority;
             ss << "_n_"     << problem_size;
-            ss << "_m_"     << std::scientific << nominal_mips;
-            ss << "_bw_"    << std::scientific << bandwidth;
-            ss << "_p_"     << std::scientific << ping;
+            ss << "_m_"     << std::scientific << std::setprecision(3) << nominal_mips;
+            ss << "_bw_"    << std::scientific << std::setprecision(3) << bandwidth;
+            ss << "_p_"     << std::scientific << std::setprecision(3) << ping;
             ss << "_rd_"    << randomize_count;
             if (single_player)
                 ss << "single";
@@ -140,7 +150,7 @@ int main(int argc, char* argv[])
         if(single_player)
         {
             file << "Slice,Time" << std::endl;
-            for (size_t i = slice_start; i < slice_end; i += slice_step)
+            for (const auto& i : slices)
             {
                 file << i << ',';
                 auto tasks = smpp::mmsim::create_tasks<task>(problem_size, { i});
@@ -161,10 +171,10 @@ int main(int argc, char* argv[])
         else
         {
             file << "Slice First,Slice Second,Time First,Time Second" << std::endl;
-            const size_t first_start    = fix_first != 0 ? fix_first        : slice_start;
-            const size_t first_end      = fix_first != 0 ? fix_first + 1    : slice_end;
-            for (size_t i = first_start; i < first_end; i += slice_step)
-                for (size_t j = slice_start; j < slice_end; j += slice_step)
+            const std::vector<size_t> v{ fix_first };
+            const std::vector<size_t>& f_s = fix_first == 0 ? slices : v;
+            for (const auto& i : f_s)
+                for (const auto&j : slices)
                 {
                     file << i << ',' << j << ',';
 
